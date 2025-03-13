39ecc27f27312d478f328f17423c3a09550dff3c
Hicrandn <19360859043@ogrenci.btu.edu.tr>
2025-03-13 16:41:56 +0300

    .

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,21 +1,23 @@
 "use client"
 
-import { z } from "zod"
+import { useState } from "react"
+import Image from "next/image"
+import Link from "next/link"
+import { set, z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-
-
 import { Button } from "./ui/button"
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "./ui/form";
 import { Input } from "./ui/input"
+import { init } from "next/dist/compiled/webpack/webpack"
+import { createAccount } from "../lib/actions/user.actions"
 
 const formSchema = z.object({
     username: z.string().min(2, {
@@ -25,42 +27,147 @@ const formSchema = z.object({
 
 type FormType = "sign-in" | "sign-up";
 
+const AuthFormSchema = (formType: "sign-up" | "sign-in") => {
+  return z.object({
+    email: z.string().email(),
+    fullName: formType === "sign-up" 
+      ? z.string().min(2).max(50) 
+      : z.string().optional(),
+  });
+};
+
 const AuthForm = ({ type}: {type:FormType}) => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [errorMessage, setErrorMessage]=useState("");
+  const [accountId, setAccountId] = useState<string | null >(null);
+
+
+   const formSchema= AuthFormSchema(type);
     
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
-      username: "",
+      fullName: "", 
+      email:"",
     },
   })
  
  
-  const onSubmit = async(values: z.infer<typeof formSchema>)=> {
-    console.log(values)
-  }
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    setIsLoading(true);
+    setErrorMessage("");
+
+    try {
+        const user = await createAccount({
+            fullName: values.fullName || "",
+            email: values.email,
+        });
+
+        setAccountId(user.accountId);
+    } catch (error) {
+        setErrorMessage("Failed to create account. Please try again.");
+    } finally {
+        setIsLoading(false);
+    }
+};
+
 
   return (
     <>
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} 
       className="auth-form">
-        <FormField
+        <h1 className="form-title"> 
+          {type === "sign-in" ? "Sign In" : "Sign Up"}
+        </h1>
+        { type === 'sign-up' && ( 
+          <FormField
           control={form.control}
-          name="username"
+          name="fullName"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <div className="shad-form-item">
+              <FormLabel className="shad-form-label">Full Name </FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="Enter your full name"
+                className="shad-input" 
+                {...field} />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
+
+
+              </div>
+              
+              <FormMessage className="shad-form-message" />
             </FormItem>
           )}
+          
         />
-        <Button type="submit">Submit</Button>
+
+        
+        
+         )}
+
+<FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <div className="shad-form-item">
+              <FormLabel className="shad-form-label">Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your email"
+                className="shad-input" 
+                {...field} />
+              </FormControl>
+
+
+              </div>
+              
+              <FormMessage className="shad-form-message" />
+            </FormItem>
+          )}
+          />
+
+         
+        <Button type="submit" className="form-submit-button" disabled={isLoading}>
+         { type === "sign-in" ? "Sign In" : "Sign Up"}
+
+         {isLoading && (
+          <Image
+           src ="/assets/icons/loader.svg"
+          alt="loading"
+          width={24}
+          height={24}
+          className="ml-2 animate-spin"
+          />
+         )}
+          </Button>
+
+          {errorMessage && (
+            <p className="error-message">
+              *{errorMessage}
+            </p>
+          )}
+
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              { type === "sign-in" 
+              ? "Don't have an account?" 
+              : "Already have an account?"}
+            </p>
+            
+            <Link href={ type === "sign-in" 
+              ? "/sign-up" 
+              : "/sign-in"}
+              className="ml-1 font-medium text-brand"
+              >
+                {""}
+                { type === "sign-in"
+                ? "Sign Up" 
+                : "Sign In"}
+            </Link>
+
+          </div>
       </form>
     </Form>
     {/* OTP VERİFİCATİON */}
