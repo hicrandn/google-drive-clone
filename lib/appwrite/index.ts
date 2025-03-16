917d7f8ba0c9baeb4a8f7653aedfe352a5f6c8e8
Hicran Apaydın <hicranapaydin@192.168.1.42>
2025-03-16 18:02:07 +0300

    Upload Files

--- lib/appwrite/index.ts
+++ lib/appwrite/index.ts
@@ -4,22 +4,15 @@ import { Account, Avatars, Client, Databases, Storage } from "node-appwrite";
 import { appwriteConfig } from "@/lib/appwrite/config";
 import { cookies } from "next/headers";
 
-// Appwrite istemcisi oluşturulması
 export const createSessionClient = async () => {
   const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
     .setProject(appwriteConfig.projectId);
 
-  // Kullanıcı oturum bilgisini çerezden al
   const session = (await cookies()).get("appwrite-session");
 
-  // Oturum verisi yoksa hata fırlat
-  if (!session || !session.value) {
-    // Oturum yoksa kullanıcıyı login sayfasına yönlendirebilirsiniz
-    throw new Error("No session");
-  }
+  if (!session || !session.value) throw new Error("No session");
 
-  // Oturum verisini Appwrite istemcisine set et
   client.setSession(session.value);
 
   return {
@@ -32,7 +25,6 @@ export const createSessionClient = async () => {
   };
 };
 
-// Yönetici erişim için istemci oluşturulması
 export const createAdminClient = async () => {
   const client = new Client()
     .setEndpoint(appwriteConfig.endpointUrl)
